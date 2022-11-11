d92fb8f41ec30439666468c4326391596b3c2744
Dave Sutter <sutter@intransix.com>
2022-11-10 20:36:01 -0800

    initial commit

---
+++ eventTypes.R
@@ -0,0 +1,168 @@
+##offcial weather type_sum
+
+offTypes <- c(
+  "Astronomical Low Tide",
+  
+  "Avalanche",
+  
+  "Flash Flood",
+  "Flood",
+  "Coastal Flood",
+  "Lakeshore Flood",
+  
+  "Thunderstorm Wind", #downburst, gustnado
+  "Heavy Rain",
+  
+  "Lightning",
+  
+  "Heavy Snow",
+  "Blizzard",
+  "Lake-Effect Snow",
+  "Sleet",
+  "Ice Storm",
+  "Winter Storm",
+  "Winter Weather",
+  
+  "Extreme Cold/Wind Chill",
+  "Frost/Freeze",
+  "Cold/Wind Chill",
+  
+  "Excessive Heat",
+  "Heat",
+  
+  "Hail",
+  
+  "Dense Fog",
+  "Freezing Fog",
+  
+  "Debris Flow",
+  
+  "High Surf",
+  
+  "Wildfire",
+  "Dense Smoke",
+  
+  "Drought",
+  
+  "Dust Devil",
+  
+  "Dust Storm",
+  
+  "Tornado",
+  "Funnel Cloud",
+  "Waterspout",
+  
+  "Hurricane (Typhoon)",
+  "Tropical Depression",
+  "Tropical Storm",
+  "High Wind",
+  "Strong Wind",
+  
+  "Tsunami",
+  "Sneaker Wave",
+  "Storm Surge/Tide",
+  
+  "Volcanic Ash",
+  
+  "Rip Current",
+  
+  "Seiche",
+  
+  "Marine Dense Fog",
+  "Marine Hail", ##"MARINE HAIL"
+  "Marine Heavy Freezing Spray",
+  "Marine Hurricane/Typhoon",
+  "Marine Lightning",
+  "Marine High Wind", ##"MARINE HIGH WIND"
+  "Marine Strong Wind", ##"MARINE STRONG WIND"
+  "Marine Thunderstorm Wind", ## "MARINE TSTM WIND", "MARINE THUNDERSTORM WIND"
+  "Marine Tropical Depression",
+  "Marine Tropical Storm"
+)
+
+
+
+keywords <- c(
+  "hurricane",
+  "typhoon",
+  "tropical",
+  "wind",
+  
+  "tornado",
+  "funnel",
+  "waterspout",
+  
+  "tsunami",
+  "surge",
+  
+  "flood",
+  "stream",
+  "water",
+  
+  "thunderstorm",
+  "rain",
+  "burst",
+  "shower",
+  
+  "lightning",
+  
+  "coastal",
+  
+  "hail",
+  
+  "snow",
+  "freezing",
+  "ice",
+  "blizzard",
+  "winter",
+  "sleet",
+  "mix",
+  "glaze",
+  "icy",
+  
+  "precipitation",
+  
+  "fire",
+  "smoke",
+  
+  "fog",
+  
+  "lighting",
+  
+  
+  "surf",
+  "seas",
+  "marine",
+  "tide",
+  "wave",
+  "current",
+  "swell",
+  
+  "heat",
+  "hot",
+  "warm",
+  
+  "cold",
+  "freeze",
+  "frost",
+  "hypothermia",
+  
+  
+  
+  "slide",
+  
+  "drought",
+  
+  "avalanche",
+  
+  "turbulence",
+  
+  "volcan",
+  
+  "dam break",
+  
+  
+  "landspout",
+  "dust"
+)
+  
---
+++ scrap.R
@@ -0,0 +1,232 @@
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(stringr) #reg exp
+
+set.seed(111)
+
+## notes on data:
+## https://www.ncdc.noaa.gov/stormevents/details.jsp
+##
+##  1950 - 1954 - only tornado
+##  1955 - 1995 - only tornado, thunderstorm wind, hail 
+##  1996 - present - 48 event types
+
+###########################################
+## FUNCTIONS
+###########################################
+
+
+##=====================================
+## This function converts exponent strings to numerica exponents
+##=====================================
+## This converts exponent string values that are
+## - a number of metric exponents - see code for selection
+## - numeric strings (convert to numeric)
+## - other - anything else is converted to an exponent of 0
+## In addition a safety text is done that there are no single letter
+## values that are not converted by the metric coefficients, with the
+## assumption that theis coefficient should be added.
+convertExpCol <- function(expStr) {
+  expNum <- numeric(length(expStr))
+  expNum <- NA
+  
+  expNum[str_detect(expStr,"[hH]")] = 2
+  expNum[str_detect(expStr,"[kK]")] = 3
+  expNum[str_detect(expStr,"[mM]")] = 6
+  expNum[str_detect(expStr,"[bB]")] = 9
+  expNum[str_detect(expStr,"[tT]")] = 12
+  
+  isnum <- str_detect(expStr,"[0-9]+")
+  expNum[isnum] = as.numeric(expStr[isnum])
+  
+  ## SAFETY TEST --------
+  ## Check for any remaining single letter exponents
+  ## These are probably metric prefixes we left out
+  singleLetterExpLeft <- sum(str_detect(expStr[is.na(expNum)],"[a-zA-Z]"))
+  if(singleLetterExpLeft > 0) {
+    warning("There are unaccounted letter values for exponent! Investigate")
+  }
+  ##--------------
+  
+  expNum[is.na(expNum)] = 0 
+  
+  expNum
+}
+
+## this returns a randomly selected subset roughly a frac of original length
+getSubset <- function(df,frac) {
+  df[as.logical(rbinom(nrow(df),size=1,prob=frac)),]
+}
+
+###########################################
+## ANALYSIS
+###########################################
+
+##=======================
+## load file
+##======================
+loadData <- function() {
+  ##load all data
+  allData <- read.csv("repdata_data_StormData.csv.bz2")
+  ##return data with damage, fatalities or injuries
+  filter(allData,(PROPDMG > 0) | (CROPDMG > 0) | (FATALITIES > 0) | (INJURIES > 0))
+}
+
+dat <- loadData()
+
+##=====================
+## update columns and classes
+##=====================
+dat$EVTYPE <- factor(dat$EVTYPE)
+dat$BGN_DATE <- mdy_hms(dat$BGN_DATE)
+
+##add a column for damage amounts
+
+##damge amount exponents
+#expString <- unique(tolower(c(unique(dat$PROPDMGEXP),unique(dat$CROPDMGEXP))))
+
+dat$PEXP <- convertExpCol(dat$PROPDMGEXP)
+dat$CEXP <- convertExpCol(dat$CROPDMGEXP)
+
+dat$DMG <- dat$PROPDMG * (10^dat$PEXP) + dat$CROPDMG * (10^dat$CEXP)
+
+datShort = getSubset(dat,.05)
+
+##analyze based on data 1996 and later.
+## long and short data
+dat96 <- dat[dat$BGN_DATE >= "1996-01-01",]
+
+datShort96 <- datShort[datShort$BGN_DATE >= "1996-01-01",]
+
+##===========================
+## totals
+##===========================
+et <- unique(dat$EVTYPE)
+
+length(et)
+
+
+
+###########################################
+## APPENDIX
+###########################################
+
+et <- as.character(unique(dat$EVTYPE))
+
+length(et)
+
+
+
+getEts <- function(regText) {
+  str_detect(et,regex(regText,ignore_case=TRUE))
+}
+
+getEtf <- function(regTexts) {
+  etf <- data.frame(et = et)
+  
+  addCol <- function(regText) {
+    etf[[regText]] <<- getEts(regText)
+  }
+  
+  sapply(regTexts,addCol)
+  
+  etf$total <- apply(etf[,2:ncol(etf)],1,sum)
+  
+  etf
+}
+
+
+
+
+etfM <- getEtf(keywords)
+etfO <- getEtf(offTypes)
+
+etfM[etfM$total == 0,"et"]
+etfO[etfO$total == 0,"et"]
+
+
+#scrap
+getUsage <- function(et,regText) {
+  et[str_detect(et,regex(regText,ignore_case=TRUE))]
+}
+
+getUsage(et,"Marine")
+
+
+##===========================
+## event types
+##===========================
+
+##===========================
+##look at data for inflation and other
+##===========================
+
+
+
+##scatter plot of all data
+## noticeable change in 90s - see data sourcing
+ggplot(dataShort,aes(BGN_DATE,log10(DMG))) + geom_point() + geom_smooth(method = "lm")
+
+##before 92 - note the increasing cost trend (not infalation adjusted?)
+ggplot(dataShort[dataShort$BGN_DATE < "1992-01-01",],aes(BGN_DATE,log(DMG))) + geom_point() + geom_smooth(method = "lm")
+
+##after 1/1/96 - cost constant over time (inflation adjusted?)
+ggplot(dataShort[dataShort$BGN_DATE > "1996-01-01",],aes(BGN_DATE,log10(DMG))) + geom_point() + geom_smooth(method = "lm")
+
+
+##=========================
+## test
+##=========================
+
+datShort <- dat[1:1000,]
+
+##https://rstudio-pubs-static.s3.amazonaws.com/58957_37b6723ee52b455990e149edde45e5b6.html
+
+convertExpCol <- function(df,targetCol,sourceCol) {
+  df[[targetCol]] <- NA
+  
+  ##metric prefixes
+  df$PEXP[str_detect(df$PROPDMGEXP,"[hH]")] = 2
+  df$PEXP[str_detect(df$PROPDMGEXP,"[kK]")] = 3
+  df$PEXP[str_detect(df$PROPDMGEXP,"[mM]")] = 6
+  df$PEXP[str_detect(df$PROPDMGEXP,"[bB]")] = 9
+  df$PEXP[str_detect(df$PROPDMGEXP,"[tT]")] = 12
+  
+  #check for any alpha values
+  singleLetterExpLeft <- sum(str_detect(datShort$PROPDMGEXP[is.na(datShort$PEXP)],"[a-zA-Z]"))
+  if(singleLetterExpLeft > 0) {
+    warning("There are unaccounted letter values for exponent! Investigate")
+  }
+  
+  ##number entries
+  isnum <- str_detect(df$PROPDMGEXP,"[0-9]+")
+  df$PEXP[isnum] = as.numeric(df$PROPDMGEXP[isnum])
+  
+  ##other - these are all 0 except "+', which is 1
+  df$PEXP[str_detect(df$PROPDMGEXP,"+")] = 1
+  df$PEXP[is.na(df$PEXP)] = 0 
+  
+  df
+}
+
+
+
+
+
+
+dataShort = data[as.logical(rbinom(nrow(data),size=1,prob=.01)),]
+
+unique(dataShort$EVTYPE)
+
+summary(dataShort$PROPDMG)
+
+
+damDataShort <- filter(dataShort,(PROPDMG > 0) | (CROPDMG > 0) | (FATALITIES > 0) | (INJURIES > 0))
+dim(damDataShort)
+
+damData <- filter(data,(PROPDMG > 0) | (CROPDMG > 0) | (FATALITIES > 0) | (INJURIES > 0))
+
+
+
+unique(damDataShort$EVTYPE)
